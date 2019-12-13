1bb38026fa9c561cf1ac897fa3e0f681e26255e4
Ian Buttimer <x14112451@student.ncirl.ie>
2019-12-13 21:14:37 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+import setuptools
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = {}
+with open(os.path.join(_here, 'version.py')) as f:
+    exec(f.read(), version)
+
+setuptools.setup(
+    name="dap_project",
+    version=version['__version__'],
+    author="Ian Buttimer & Philippe Tap",
+    author_email="author@example.com",
+    description="DAP project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ib-da-ncirl/sales_journal",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+      'psycopg2>=2.8.4',
+      'pymongo>=3.9.0',
+      'dagster>=0.6.4',
+      'dagit>=0.6.4',
+      'dagster_pandas>=0.6.4',
+      'pandas>=0.25.3'
+    ],
+    dependency_links=[
+        'git+https://github.com/ib-da-ncirl/db_toolkit.git#egg=db_toolkit',
+        'git+https://github.com/ib-da-ncirl/dagster_toolkit.git#egg=dagster_toolkit',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ version.py
@@ -0,0 +1,4 @@
+"""
+Overall version of the application
+"""
+__version__ = '0.0.1.dev0'
